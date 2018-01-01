94af66492f8a836f7dd33e2fe7dd5fd1f09d368a
Isaias Valle <guitartsword@gmail.com>
2018-01-01 13:55:58 -0600

    updated max level

--- scripts/stored_procedures/th_set_level.sql
+++ scripts/stored_procedures/th_set_level.sql
@@ -3,7 +3,7 @@ CREATE PROCEDURE `th_set_level` (
     lvl TINYINT UNSIGNED
 )
 BEGIN
-	SET @max_level = (SELECT max_level from TownHall where townhall_id = 'validth1');
+	SET @max_level = (SELECT MAX(level) FROM TownHall WHERE townhall_id LIKE 'validth%');
 	IF lvl<1 OR lvl > @max_level THEN
 		SIGNAL SQLSTATE '45000' 
 			SET MESSAGE_TEXT = 'INVALID townhall level';
